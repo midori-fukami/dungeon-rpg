49cd012680daa465d48c1f61aa3dfc405e9fce62
Midori Fukami <midori.fukami@hotmail.com>
2024-08-12 22:09:26 +0200

    Add enemy attack area

--- Dungeon RPG/Scripts/Characters/Enemy/EnemyAttackState.cs
+++ Dungeon RPG/Scripts/Characters/Enemy/EnemyAttackState.cs
@@ -1,10 +1,63 @@
 using Godot;
 using System;
+using System.Linq;
 
 public partial class EnemyAttackState : EnemyState
 {
+    private Vector3 targetPosition;
+
     protected override void EnterState()
     {
         characterNode.AnimationPlayerNode.Play(GameConstants.ANIM_ATTACK);
+
+        Node3D target = characterNode.AttackAreaNode
+            .GetOverlappingBodies()
+            .First();
+
+        targetPosition = target.GlobalPosition;
+        characterNode.AnimationPlayerNode.AnimationFinished += HandleAnimationFinished;
+    }
+
+    protected override void ExitState()
+    {
+        characterNode.AnimationPlayerNode.AnimationFinished -= HandleAnimationFinished;
+    }
+
+    private void HandleAnimationFinished(StringName animName)
+    {
+        characterNode.ToggleHitBox(true);
+
+        Node3D target = characterNode.AttackAreaNode
+            .GetOverlappingBodies()
+            .FirstOrDefault();
+
+        if (target == null)
+        {
+            Node3D chaseTarget = characterNode.ChaseAreaNode
+                .GetOverlappingBodies()
+                .FirstOrDefault();
+
+            if (chaseTarget == null)
+            {
+                characterNode.StateMachineNode.SwitchState<EnemyReturnState>();
+                return;
+            }
+            characterNode.StateMachineNode.SwitchState<EnemyChaseState>();
+            return;
+        }
+
+        characterNode.AnimationPlayerNode.Play(GameConstants.ANIM_ATTACK);
+        targetPosition = target.GlobalPosition;
+
+        Vector3 direction = characterNode.GlobalPosition
+            .DirectionTo(targetPosition);
+        characterNode.SpriteNode.FlipH = direction.X < 0;
+        
+    }
+
+    private void PerformHit()
+    {
+        characterNode.ToggleHitBox(false);
+        characterNode.HitboxNode.GlobalPosition = targetPosition;
     }
 }
