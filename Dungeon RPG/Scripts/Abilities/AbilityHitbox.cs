7eba6a8068ed7c543c101fd2ab3105f68c4a5fd2
Midori Fukami <midori.fukami@hotmail.com>
2025-05-22 22:00:59 +0200

    Adding stun

--- Dungeon RPG/Scripts/Abilities/AbilityHitbox.cs
+++ Dungeon RPG/Scripts/Abilities/AbilityHitbox.cs
@@ -3,5 +3,11 @@ using System;
 
 public partial class AbilityHitbox : Area3D, IHitbox
 {
+    public bool CanStun()
+    {
+        return true;
+    }
+
+
     public float GetDamage() => GetOwner<Ability>().Damage;
 }
--- Dungeon RPG/Scripts/Characters/Enemy/Enemy.cs
+++ Dungeon RPG/Scripts/Characters/Enemy/Enemy.cs
@@ -5,6 +5,20 @@ public partial class Enemy : Character
 {
     public override void _Ready()
     {
-        base._Ready();        
+        base._Ready();
+
+        HurtboxNode.AreaEntered += HandleAreaEntered;
+    }
+
+    private void HandleAreaEntered(Area3D area)
+    {
+        if (area is not IHitbox hitbox)
+        {
+            return;
+        }
+
+        if (hitbox.CanStun() && GetStatResource(Stat.Health).StatValue != 0) {
+            
+        }
     }
 }
